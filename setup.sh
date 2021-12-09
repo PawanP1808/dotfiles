30437a62d384e2afe12089696dc01dcb1f5a2303
Pawan Patel <pawan.patel@shopify.com>
2021-12-09 15:40:12 -0500

    last try

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 if [ -d ~/.oh-my-zsh ]; then
 	echo "oh-my-zsh is installed"
  else
- 	./ohmyzsh
+    echo "installing oh my zsh"
+ 	sh ./ohmy.sh
 fi
-
+echo "DONE "
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 git config --global alias.l "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.st status
